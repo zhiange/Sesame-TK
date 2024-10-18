929ab4171238fef0839ebaa80800c45a4df66521
Byseven <2104898527@qq.com>
2024-10-18 18:17:09 +0800

    :art:调整默认使用双击卡时间范围为"0000-2359"。

--- app/src/main/java/tkaxv7s/xposed/sesame/model/normal/base/BaseModel.java
+++ app/src/main/java/tkaxv7s/xposed/sesame/model/normal/base/BaseModel.java
@@ -28,7 +28,7 @@ public class BaseModel extends Model {
     @Getter
     private static final ListModelField.ListJoinCommaToStringModelField wakenAtTimeList = new ListModelField.ListJoinCommaToStringModelField("wakenAtTimeList", "定时唤醒(关闭:-1)", ListUtil.newArrayList("0650", "2350"));
     @Getter
-    private static final ListModelField.ListJoinCommaToStringModelField energyTime = new ListModelField.ListJoinCommaToStringModelField("energyTime", "只收能量时间(范围)", ListUtil.newArrayList("0700-0731"));
+    private static final ListModelField.ListJoinCommaToStringModelField energyTime = new ListModelField.ListJoinCommaToStringModelField("energyTime", "只收能量时间(范围)", ListUtil.newArrayList("0000-2359"));
     @Getter
     private static final ChoiceModelField timedTaskModel = new ChoiceModelField("timedTaskModel", "定时任务模式", TimedTaskModel.SYSTEM, TimedTaskModel.nickNames);
     @Getter
