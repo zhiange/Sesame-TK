28bba792737fcf28b030a8999bd94d83393e7f05
Byseven <2104898527@qq.com>
2024-11-26 15:25:46 +0800

    修改导入的工具类名称，并更新相关方法调用以使用新的工具类。

--- app/src/main/java/fansirsqi/xposed/sesame/entity/CooperateUser.java
+++ app/src/main/java/fansirsqi/xposed/sesame/entity/CooperateUser.java
@@ -1,6 +1,6 @@
 package fansirsqi.xposed.sesame.entity;
 
-import fansirsqi.xposed.sesame.util.CooperationIdMap;
+import fansirsqi.xposed.sesame.util.CooperationIdMapUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +28,7 @@ public class CooperateUser extends IdAndName {
      */
     public static List<CooperateUser> getList() {
         List<CooperateUser> list = new ArrayList<>();
-        Set<Map.Entry<String, String>> idSet = CooperationIdMap.getMap().entrySet();
+        Set<Map.Entry<String, String>> idSet = CooperationIdMapUtil.getMap().entrySet();
 
         // 遍历映射表的每一项，添加到用户列表中
         for (Map.Entry<String, String> entry : idSet) {
