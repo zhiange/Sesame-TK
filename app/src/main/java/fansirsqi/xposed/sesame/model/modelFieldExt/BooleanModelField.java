8c2ba3d358e5939f1f45f56ccec77a751228947e
Byseven <2104898527@qq.com>
2024-11-26 15:24:15 +0800

    重命名 BooleanModelField 类的包路径，从 data.modelFieldExt 修改为 model.modelFieldExt，并相应更新了导入语句。

--- app/src/main/java/fansirsqi/xposed/sesame/model/modelFieldExt/BooleanModelField.java
+++ app/src/main/java/fansirsqi/xposed/sesame/model/modelFieldExt/BooleanModelField.java
@@ -1,11 +1,11 @@
-package fansirsqi.xposed.sesame.data.modelFieldExt;
+package fansirsqi.xposed.sesame.model.modelFieldExt;
 
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Switch;
-import fansirsqi.xposed.sesame.data.ModelField;
+import fansirsqi.xposed.sesame.model.ModelField;
 
 public class BooleanModelField extends ModelField<Boolean> {
 
