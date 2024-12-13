19616d416f32e4a595422554ef0c29220a869d7c
Byseven <2104898527@qq.com>
2024-12-13 15:32:21 +0800

    统一使用`ContextCompat.getDrawable(context, R.drawable.button)`替代`context.getResources().getDrawable(R.drawable.button)`来设置按钮背景。

--- app/src/main/java/fansirsqi/xposed/sesame/model/modelFieldExt/SelectAndCountOneModelField.java
+++ app/src/main/java/fansirsqi/xposed/sesame/model/modelFieldExt/SelectAndCountOneModelField.java
@@ -51,7 +51,7 @@ public class SelectAndCountOneModelField extends ModelField<KVNode<String, Integ
         btn.setText(getName());
         btn.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         btn.setTextColor(ContextCompat.getColor(context, R.color.button));
-        btn.setBackground(context.getResources().getDrawable(R.drawable.button));
+        btn.setBackground(ContextCompat.getDrawable(context, R.drawable.button));
         btn.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
         btn.setMinHeight(150);
         btn.setPaddingRelative(40, 0, 40, 0);
