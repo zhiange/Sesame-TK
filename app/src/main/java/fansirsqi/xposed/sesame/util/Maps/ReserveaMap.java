e7dcfaccf14f84c18430e0cc7e6e108efe9dbe50
Byseven <2104898527@qq.com>
2024-12-30 11:16:17 +0800

    feat: 新增抽象ID映射工具类及其子类实现

---
+++ app/src/main/java/fansirsqi/xposed/sesame/util/Maps/ReserveaMap.java
@@ -0,0 +1,8 @@
+package fansirsqi.xposed.sesame.util.Maps;
+
+public class ReserveaMap extends IdMapManager {
+    @Override
+    public String thisFileName() {
+        return "ReserveaMap.json";//保护地ID映射表
+    }
+}
