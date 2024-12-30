75934bfb0250372547ddab614ab7821ff2beb38b
Byseven <2104898527@qq.com>
2024-12-30 11:09:28 +0800

    feat: 新增活力值兑换实体类及对应映射类

---
+++ app/src/main/java/fansirsqi/xposed/sesame/util/Maps/VitalityRewardsMap.java
@@ -0,0 +1,10 @@
+package fansirsqi.xposed.sesame.util.Maps;
+
+public class VitalityRewardsMap extends IdMapManager{
+
+    @Override
+    public String thisFileName(){
+        return "vitalityRewardsMap.json";//活力值兑换映射表
+    }
+
+}
