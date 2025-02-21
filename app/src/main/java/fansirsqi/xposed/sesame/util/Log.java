d0c0a92f087f988c43bad2dfd767b1f1cc4365c6
Byseven <2104898527@qq.com>
2025-02-21 18:32:19 +0800

    refactor: 修改日志TAG的生成方式并统一格式

--- app/src/main/java/fansirsqi/xposed/sesame/util/Log.java
+++ app/src/main/java/fansirsqi/xposed/sesame/util/Log.java
@@ -1,13 +1,16 @@
 package fansirsqi.xposed.sesame.util;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import fansirsqi.xposed.sesame.BuildConfig;
 import fansirsqi.xposed.sesame.model.BaseModel;
+
 /**
  * 日志工具类，负责初始化和管理各种类型的日志记录器，并提供日志输出方法。
  */
 public class Log {
-    private static final String TAG = "[" + BuildConfig.VERSION_CODE + BuildConfig.BUILD_NUMBER + "]";
+    private static final String TAG = "[" + BuildConfig.BUILD_TAG + BuildConfig.BUILD_NUMBER + "]";
     private static final Logger RUNTIME_LOGGER;
     private static final Logger SYSTEM_LOGGER;
     private static final Logger RECORD_LOGGER;
@@ -17,6 +20,7 @@ public class Log {
     private static final Logger OTHER_LOGGER;
     private static final Logger ERROR_LOGGER;
     private static final Logger CAPTURE_LOGGER;
+
     static {
         Logback.configureLogbackDirectly();
         RUNTIME_LOGGER = LoggerFactory.getLogger("runtime");
@@ -29,62 +33,78 @@ public class Log {
         ERROR_LOGGER = LoggerFactory.getLogger("error");
         CAPTURE_LOGGER = LoggerFactory.getLogger("capture");
     }
+
     private static String truncateLogMessage(String message) {
         if (message.length() > 16) {
             return message.substring(0, 16) + "...";
         }
         return message;
     }
+
     public static void system(String message) {
         SYSTEM_LOGGER.info(TAG + "{}", message);
     }
+
     public static void system(String TAG, String message) {
         system("[" + TAG + "]: " + message);
     }
+
     public static void runtime(String message) {
         system(message);
         RUNTIME_LOGGER.info(TAG + "{}", message);
     }
+
     public static void runtime(String TAG, String message) {
         runtime("[" + TAG + "]: " + message);
     }
+
     public static void record(String message) {
         runtime(message);
         if (BaseModel.getRecordLog().getValue()) {
             RECORD_LOGGER.info(TAG + "{}", message);
         }
     }
+
     public static void record(String TAG, String message) {
         record("[" + TAG + "]: " + message);
     }
+
     public static void forest(String message) {
         record(message);
         FOREST_LOGGER.info("{}", message);
     }
+
     public static void forest(String TAG, String message) {
         forest("[" + TAG + "]: " + message);
     }
+
     public static void farm(String message) {
         record(message);
         FARM_LOGGER.info("{}", message);
     }
+
     public static void farm(String TAG, String message) {
         farm("[" + TAG + "]: " + message);
     }
+
     public static void other(String message) {
         record(message);
         OTHER_LOGGER.info("{}", message);
     }
+
     public static void other(String TAG, String message) {
         other("[" + TAG + "]: " + message);
     }
+
     public static void debug(String message) {
         runtime(message);
         DEBUG_LOGGER.info("{}", message);
     }
+
     public static void debug(String TAG, String message) {
         debug("[" + TAG + "]: " + message);
     }
+
     public static void error(String message) {
         if (BaseModel.getErrNotify().getValue()) {
             Notify.sendErrorNotification("‼️芝麻粒运行时发生异常，详情查看异常日志", message);
@@ -92,30 +112,37 @@ public class Log {
         runtime(message);
         ERROR_LOGGER.error(TAG + "{}", message);
     }
+
     public static void error(String TAG, String message) {
         error("[" + TAG + "]: " + message);
     }
+
     public static void capture(String message) {
         CAPTURE_LOGGER.info(TAG + "{}", message);
     }
+
     public static void capture(String TAG, String message) {
         capture("[" + TAG + "]: " + message);
     }
+
     public static void printStackTrace(Throwable th) {
         String stackTrace = "error: " + android.util.Log.getStackTraceString(th);
         error(stackTrace);
         runtime(stackTrace);
     }
+
     public static void printStackTrace(String TAG, Throwable th) {
         String stackTrace = "Throwable error: " + android.util.Log.getStackTraceString(th);
         error(TAG, stackTrace);
         runtime(TAG, stackTrace);
     }
+
     public static void printStackTrace(Exception e) {
         String stackTrace = "Exception error: " + android.util.Log.getStackTraceString(e);
         error(stackTrace);
         runtime(stackTrace);
     }
+
     public static void printStackTrace(String TAG, Exception e) {
         String stackTrace = "Throwable error: " + android.util.Log.getStackTraceString(e);
         error(TAG, stackTrace);
