1115478af507a9f4d081d3909319904d0a05e812
bocchi810 <admin@haoming9245.top>
2025-05-03 10:56:48 +0800

    feat: 日常强迫症

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,12 +104,12 @@ android {
         when (name) {
             "normal" -> {
                 compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_23
-                    targetCompatibility = JavaVersion.VERSION_23
+                    sourceCompatibility = JavaVersion.VERSION_24
+                    targetCompatibility = JavaVersion.VERSION_24
                 }
                 kotlin {
                     compilerOptions {
-                        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_23
+                        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24
                     }
                 }
             }
