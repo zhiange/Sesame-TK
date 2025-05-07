0869338287707f3b1a005af83e49db6d8ddebb27
byseven <2104898527@qq.com>
2025-05-07 17:50:11 +0800

    fix: 修复光盘行动图片缓存问题 refactor: 优化DataCache和ExtendActivity结构，添加扩展不再需要写xml那么勾八繁琐,直接在extendFunctions.add接口添加

--- app/src/main/java/fansirsqi/xposed/sesame/ui/ExtendActivity.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/ui/ExtendActivity.kt
@@ -1,13 +1,18 @@
 package fansirsqi.xposed.sesame.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fansirsqi.xposed.sesame.R
 import fansirsqi.xposed.sesame.data.DataCache
+import fansirsqi.xposed.sesame.data.ViewAppInfo
+import fansirsqi.xposed.sesame.entity.ExtendFunctionItem
+import fansirsqi.xposed.sesame.ui.widget.ExtendFunctionAdapter
+import fansirsqi.xposed.sesame.util.FansirsqiUtil
 import fansirsqi.xposed.sesame.util.Log
 import fansirsqi.xposed.sesame.util.ToastUtil
 
@@ -16,6 +21,9 @@ import fansirsqi.xposed.sesame.util.ToastUtil
  */
 class ExtendActivity : BaseActivity() {
     private var debugTips: String? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var extendFunctionAdapter: ExtendFunctionAdapter
+    private val extendFunctions = mutableListOf<ExtendFunctionItem>()
 
     /**
      * 初始化Activity
@@ -26,48 +34,99 @@ class ExtendActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extend) // 设置布局文件
         debugTips = getString(R.string.debug_tips)
-        // 初始化按钮并设置点击事件
-        initButtonsAndSetListeners()
-    }
-
-    /**
-     * 初始化按钮并设置监听器
-     */
-    private fun initButtonsAndSetListeners() {
-        // 定义按钮变量并绑定按钮到对应的View
-        val btnGetTreeItems = findViewById<Button>(R.id.get_tree_items)
-        val btnGetNewTreeItems = findViewById<Button>(R.id.get_newTree_items)
-        //完善下面这两个按钮对应功能
-        val btnQueryAreaTrees = findViewById<Button>(R.id.query_area_trees)
-        val btnGetUnlockTreeItems = findViewById<Button>(R.id.get_unlock_treeItems)
-
-        val btnclearphotoGuangPan = findViewById<Button>(R.id.clear_photo)
-
-        // 设置Activity标题
         baseTitle = getString(R.string.extended_func)
-        // 为每个按钮设置点击事件
-        btnGetTreeItems.setOnClickListener(TreeItemsOnClickListener())
-        btnGetNewTreeItems.setOnClickListener(NewTreeItemsOnClickListener())
-        btnQueryAreaTrees.setOnClickListener(AreaTreesOnClickListener())
-        btnGetUnlockTreeItems.setOnClickListener(UnlockTreeItemsOnClickListener())
-        btnclearphotoGuangPan.setOnClickListener {
-            val context = this
-            AlertDialog.Builder(context)
-                .setTitle(R.string.clear_photo)
-                .setMessage("确认清空${DataCache.guangPanPhotoCount}组光盘行动图片？")
-                .setPositiveButton(R.string.ok) { dialog, which ->
-                    if (DataCache.clearGuangPanPhoto()) {
-                        ToastUtil.showToast(context, "光盘行动图片清空成功")
-                    } else {
-                        ToastUtil.showToast(context, "光盘行动图片清空失败")
-                    }
-                }
-                .setNegativeButton(R.string.cancel) { dialog, which -> dialog.dismiss() }
-                .show()
-        }
+
+        setupRecyclerView()
+        populateExtendFunctions()
     }
 
+    private fun setupRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView_extend_functions)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        extendFunctionAdapter = ExtendFunctionAdapter(extendFunctions)
+        recyclerView.adapter = extendFunctionAdapter
+    }
 
+    @SuppressLint("NotifyDataSetChanged")
+    private fun populateExtendFunctions() {
+        extendFunctions.add(
+            ExtendFunctionItem(getString(R.string.query_the_remaining_amount_of_saplings)) {
+                sendItemsBroadcast("getTreeItems")
+                ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
+            }
+        )
+        extendFunctions.add(
+            ExtendFunctionItem(getString(R.string.search_for_new_items_on_saplings)) {
+                sendItemsBroadcast("getNewTreeItems")
+                ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
+            }
+        )
+        extendFunctions.add(
+            ExtendFunctionItem(getString(R.string.search_for_unlocked_regions)) {
+                sendItemsBroadcast("queryAreaTrees")
+                ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
+            }
+        )
+        extendFunctions.add(
+            ExtendFunctionItem(getString(R.string.search_for_unlocked_items)) {
+                sendItemsBroadcast("getUnlockTreeItems")
+                ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
+            }
+        )
+        extendFunctions.add(
+            ExtendFunctionItem(getString(R.string.clear_photo)) {
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.clear_photo)
+                    .setMessage("确认清空${DataCache.guangPanPhotoCount}组光盘行动图片？")
+                    .setPositiveButton(R.string.ok) { _, _ ->
+                        if (DataCache.clearGuangPanPhoto()) {
+                            ToastUtil.showToast(this, "光盘行动图片清空成功")
+                        } else {
+                            ToastUtil.showToast(this, "光盘行动图片清空失败")
+                        }
+                    }
+                    .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
+                    .show()
+            }
+        )
+        //调试功能往里加
+        if(ViewAppInfo.isApkInDebug){
+            extendFunctions.add(
+                ExtendFunctionItem("写入光盘") {
+                    val photos = HashMap<String, String>()
+                    AlertDialog.Builder(this)
+                        .setTitle("Test")
+                        .setMessage("xxxx")
+                        .setPositiveButton(R.string.ok) { _, _ ->
+                            val randomStr = FansirsqiUtil.getRandomString(10)
+                            photos["before"] = "before$randomStr"
+                            photos["after"] = "after$randomStr"
+                            if (DataCache.saveGuangPanPhoto(photos)) {
+                                ToastUtil.showToast(this, "写入成功$photos")
+                            } else {
+                                ToastUtil.showToast(this, "写入失败$photos")
+                            }
+                        }
+                        .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
+                        .show()
+                }
+            )
+
+            extendFunctions.add(
+                ExtendFunctionItem("展示随机光盘") {
+                    AlertDialog.Builder(this)
+                        .setTitle("看看效果")
+                        .setMessage("xxxx")
+                        .setPositiveButton(R.string.ok) { _, _ ->
+                            ToastUtil.showToast(this, "${DataCache.randomGuangPanPhoto}")
+                        }
+                        .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
+                        .show()
+                }
+            )
+        }
+        extendFunctionAdapter.notifyDataSetChanged()
+    }
 
     /**
      * 发送广播事件
@@ -81,45 +140,5 @@ class ExtendActivity : BaseActivity() {
         intent.putExtra("type", type)
         sendBroadcast(intent) // 发送广播
         Log.debug("扩展工具主动调用广播查询📢：$type")
-    }
-
-    /**
-     * 获取树项目按钮的点击监听器
-     */
-    private inner class TreeItemsOnClickListener : View.OnClickListener {
-        override fun onClick(v: View) {
-            sendItemsBroadcast("getTreeItems")
-            ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    /**
-     * 获取新树项目按钮的点击监听器
-     */
-    private inner class NewTreeItemsOnClickListener : View.OnClickListener {
-        override fun onClick(v: View) {
-            sendItemsBroadcast("getNewTreeItems")
-            ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    /**
-     * 查询未解锁🔓地区
-     */
-    private inner class AreaTreesOnClickListener : View.OnClickListener {
-        override fun onClick(v: View) {
-            sendItemsBroadcast("queryAreaTrees")
-            ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    /**
-     * 查询未解锁🔓🌳木项目
-     */
-    private inner class UnlockTreeItemsOnClickListener : View.OnClickListener {
-        override fun onClick(v: View) {
-            sendItemsBroadcast("getUnlockTreeItems")
-            ToastUtil.makeText(this@ExtendActivity, debugTips, Toast.LENGTH_SHORT).show()
-        }
     }
 }
