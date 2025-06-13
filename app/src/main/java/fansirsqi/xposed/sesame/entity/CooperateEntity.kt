95ef004720eb8ec94c4b2335d044b547e07dfdd6
byseven <2104898527@qq.com>
2025-06-13 18:39:04 +0800

    refactor: 将 CooperateEntity 从 Java 转换为 Kotlin 并优化代码

--- app/src/main/java/fansirsqi/xposed/sesame/entity/CooperateEntity.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/entity/CooperateEntity.kt
@@ -1,32 +1,22 @@
 package fansirsqi.xposed.sesame.entity;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import fansirsqi.xposed.sesame.util.Maps.CooperateMap;
-import fansirsqi.xposed.sesame.util.Maps.IdMapManager;
+
+import fansirsqi.xposed.sesame.util.maps.CooperateMap;
+import fansirsqi.xposed.sesame.util.maps.IdMapManager;
+
 /**
  * 表示合作用户的实体类，包含 ID 和名称。
  */
-public class CooperateEntity extends MapperEntity {
-    /**
-     * 构造方法，根据给定的 ID 和名称初始化合作用户对象。
-     * @param i 用户的 ID
-     * @param n 用户的名称
-     */
-    public CooperateEntity(String i, String n) {
-        id = i;
-        name = n;
+class CooperateEntity(i: String, n: String) : MapperEntity() {
+    init {
+        id = i
+        name = n
     }
-    /**
-     * 获取所有合作用户的列表。
-     * @return 包含所有合作用户的 CooperateUser 对象列表
-     */
-    public static List<CooperateEntity> getList() {
-        List<CooperateEntity> list = new ArrayList<>();
-        Map<String, String> idSet = IdMapManager.getInstance(CooperateMap.class).getMap();
-        for (Map.Entry<String, String> entry : idSet.entrySet()) {
-            list.add(new CooperateEntity(entry.getKey(), entry.getValue()));
+
+    companion object {
+        fun getList(): List<CooperateEntity> {
+            return IdMapManager.getInstance(CooperateMap::class.java).map
+                .map { (key, value) -> CooperateEntity(key, value) }
         }
-        return list;
     }
+
 }
