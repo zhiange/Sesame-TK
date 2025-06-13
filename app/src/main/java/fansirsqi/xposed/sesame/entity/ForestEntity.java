466590a5bd2ac6cacec79f1a9035f231f0213b2d
byseven <2104898527@qq.com>
2025-06-13 18:38:18 +0800

    refactor: 删除ForestEntity类

--- app/src/main/java/fansirsqi/xposed/sesame/entity/ForestEntity.java
+++
@@ -1,9 +0,0 @@
-package fansirsqi.xposed.sesame.entity;
-import java.util.Arrays;
-import java.util.List;
-public class ForestEntity extends MapperEntity{
-    public ForestEntity(String i, String n) {
-        this.id = i;
-        this.name = n;
-    }
-}
