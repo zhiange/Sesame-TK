7ef64db9c0788e818d3ab3d997eacceb6fa152e7
byseven <2104898527@qq.com>
2025-06-18 16:10:53 +0800

    format

--- app/src/main/java/fansirsqi/xposed/sesame/task/antFarm/DadaDailyRpcCall.java
+++ app/src/main/java/fansirsqi/xposed/sesame/task/antFarm/DadaDailyRpcCall.java
@@ -1,5 +1,7 @@
 package fansirsqi.xposed.sesame.task.antFarm;
+
 import fansirsqi.xposed.sesame.hook.RequestManager;
+
 /**
  * @author Constanline
  * @since 2023/08/04
@@ -9,9 +11,9 @@ public class DadaDailyRpcCall {
         return RequestManager.requestString("com.alipay.reading.game.dadaDaily.home",
                 "[{\"activityId\":" + activityId + ",\"dadaVersion\":\"1.3.0\",\"version\":1}]");
     }
+
     public static String submit(String activityId, String answer, Long questionId) {
         return RequestManager.requestString("com.alipay.reading.game.dadaDaily.submit",
-                "[{\"activityId\":" + activityId + ",\"answer\":\"" + answer + "\",\"dadaVersion\":\"1.3.0\",\"questionId\":" +
-                        questionId + ",\"version\":1}]");
+                "[{\"activityId\":" + activityId + ",\"answer\":\"" + answer + "\",\"dadaVersion\":\"1.3.0\",\"questionId\":" + questionId + ",\"version\":1}]");
     }
 }
