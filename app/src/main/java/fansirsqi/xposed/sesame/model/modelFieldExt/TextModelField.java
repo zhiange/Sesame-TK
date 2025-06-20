75abd62967bf320219faf493e07bf2bda8860543
byseven <2104898527@qq.com>
2025-06-20 17:26:39 +0800

    style(modelFieldExt): 更改按钮背景资源

--- app/src/main/java/fansirsqi/xposed/sesame/model/modelFieldExt/TextModelField.java
+++ app/src/main/java/fansirsqi/xposed/sesame/model/modelFieldExt/TextModelField.java
@@ -44,7 +44,7 @@ public class TextModelField extends ModelField<String> {
         btn.setText(getName());
         btn.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         btn.setTextColor(ContextCompat.getColor(context, R.color.selection_color));
-        btn.setBackground(ContextCompat.getDrawable(context, R.drawable.button));
+        btn.setBackground(ContextCompat.getDrawable(context, R.drawable.dialog_list_button));
         btn.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
         btn.setMinHeight(150);
         btn.setMaxHeight(180);
@@ -71,7 +71,7 @@ public class TextModelField extends ModelField<String> {
             btn.setText(getName());
             btn.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             btn.setTextColor(ContextCompat.getColor(context, R.color.selection_color));
-            btn.setBackground(ContextCompat.getDrawable(context, R.drawable.button));
+            btn.setBackground(ContextCompat.getDrawable(context, R.drawable.dialog_list_button));
             btn.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
             btn.setMinHeight(150);
             btn.setMaxHeight(180);
