414608a82e8e93191e6b3ae3d54d7b9030e032e6
byseven <2104898527@qq.com>
2025-06-26 11:43:03 +0800

    feat: 迁移小鸡家庭选项功能菜单

--- app/src/main/java/fansirsqi/xposed/sesame/entity/OtherEntity.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/entity/OtherEntity.kt
@@ -19,4 +19,17 @@ object OtherEntityProvider {
         OtherEntity("FEEDS", "绿色医疗💉"),
         OtherEntity("BILL", "电子小票🎫")
     )
+
+    @JvmStatic
+    fun farmFamilyOption():List<OtherEntity> = listOf(
+        OtherEntity("familySign", "每日签到📅"),
+        OtherEntity("assignRights", "使用顶梁柱特权👷‍♂️"),
+        OtherEntity("familyClaimReward", "领取奖励🏆️"),
+        OtherEntity("feedFamilyAnimal", "帮喂小鸡🐔"),
+        OtherEntity("eatTogetherConfig", "请吃美食🍲"),
+        OtherEntity("deliverMsgSend", "道早安🌞"),
+//        OtherEntity("inviteFriendVisitFamily", "好友分享🙆‍♂️"),
+//        OtherEntity("familyDrawInfo", "开扭蛋🩲"),
+//        OtherEntity("batchInviteP2P", "串门送扭蛋🥚")
+    )
 }
